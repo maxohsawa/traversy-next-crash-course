25289725f0b64e7fef2532633fb914deaa176633
maxohsawa <max.ohsawa@gmail.com>
2021-11-03 20:40:04 -0400

    brings demo up to working level

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,9 @@
+import Meta from '../components/Meta'
+
 const about = () => {
   return (
     <div>
-      <Head>
-        <title>About</title>
-      </Head>
+      <Meta title='About' />
       <h1>About</h1>
     </div>
   )
